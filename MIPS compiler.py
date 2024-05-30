72702939240d9be5900ece20b164d075ecfbdf02
Kabir <167163594+KabC03@users.noreply.github.com>
2024-05-31 04:07:45 +1000

    Update MIPS compiler.py

--- MIPS compiler.py
+++ MIPS compiler.py
@@ -9,6 +9,7 @@ ALUaccumulator = 25 #Register 25 holds accumulator vals
 ALUHold = 24 #For division and multuolication
 indexHold = 23 #array indexing
 memReg = 22 #Array defining
+ALUholdMove = 21 #moving low and high
 wordSize = 4 #4 bytes
 
 
@@ -263,14 +264,18 @@ def run(sourceFile):
                                         destFile.write("\tsub $" + str(ALUaccumulator) + " $"+ str(ALUaccumulator) + " $"+ str(indexHold) + "\n")
                                     if prevOp == "*":
                                         destFile.write("\tmul $" + str(ALUaccumulator) + " $"+ str(indexHold) + "\n")
-                                        destFile.write("\tmflo $" + str(ALUaccumulator) + "\n")
+                                        destFile.write("\tmflo $" + str(ALUholdMove) + "\n")
+                                        destFile.write("\tadd $" + str(ALUaccumulator) + " $"+ str(ALUholdMove) + " $"+ str(ALUaccumulator) + "\n")
+
                                     elif prevOp == "/":
                                         destFile.write("\tdiv $" + str(ALUaccumulator) + " $"+ str(indexHold) + "\n")
-                                        destFile.write("\tmflo $" + str(ALUaccumulator) + "\n")
+                                        destFile.write("\tmflo $" + str(ALUholdMove) + "\n")
+                                        destFile.write("\tadd $" + str(ALUaccumulator) + " $"+ str(ALUholdMove) + " $"+ str(ALUaccumulator) + "\n")
 
                                     elif prevOp == "%":
                                         destFile.write("\tdiv $" + str(ALUaccumulator) + " $"+ str(indexHold) + "\n")
-                                        destFile.write("\tmfhi $" + str(ALUaccumulator) + "\n")
+                                        destFile.write("\tmfhi $" + str(ALUholdMove) + "\n")
+                                        destFile.write("\tadd $" + str(ALUaccumulator) + " $"+ str(ALUholdMove) + " $"+ str(ALUaccumulator) + "\n")
 
                                 
                                 else:
@@ -289,14 +294,17 @@ def run(sourceFile):
                                 destFile.write("\tsub $" + str(ALUaccumulator) + " $"+ str(ALUaccumulator) + " $"+ str(regDict[tokens[token]]) + "\n")
                             if prevOp == "*":
                                 destFile.write("\tmul $" + str(ALUaccumulator) + " $"+ str(regDict[tokens[token]]) + "\n")
-                                destFile.write("\tmflo $" + str(ALUaccumulator) + "\n")
+                                destFile.write("\tmflo $" + str(ALUholdMove) + "\n")
+                                destFile.write("\tadd $" + str(ALUaccumulator) + " $"+ str(ALUholdMove) + " $"+ str(ALUaccumulator) + "\n")
                             elif prevOp == "/":
                                 destFile.write("\tdiv $" + str(ALUaccumulator) + " $"+ str(regDict[tokens[token]]) + "\n")
-                                destFile.write("\tmflo $" + str(ALUaccumulator) + "\n")
+                                destFile.write("\tmflo $" + str(ALUholdMove) + "\n")
+                                destFile.write("\tadd $" + str(ALUaccumulator) + " $"+ str(ALUholdMove) + " $"+ str(ALUaccumulator) + "\n")
 
                             elif prevOp == "%":
                                 destFile.write("\tdiv $" + str(ALUaccumulator) + " $"+ str(regDict[tokens[token]]) + "\n")
-                                destFile.write("\tmfhi $" + str(ALUaccumulator) + "\n")
+                                destFile.write("\tmfhi $" + str(ALUholdMove) + "\n")
+                                destFile.write("\tadd $" + str(ALUaccumulator) + " $"+ str(ALUholdMove) + " $"+ str(ALUaccumulator) + "\n")
 
 
                         else: #Must be a number, check if actually a number first rhough
